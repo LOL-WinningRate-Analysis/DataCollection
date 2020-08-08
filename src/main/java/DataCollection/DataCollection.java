604a0323bb6984e1be710054a181cca536c447d3
dongjae <dhehdwo7@ajou.ac.kr>
2020-08-09 08:52:47 +0900

    Fix a typing error

--- src/main/java/DataCollection/DataCollection.java
+++ src/main/java/DataCollection/DataCollection.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DataColection {
+public class DataCollection {
 
 	private final String MY_CONSTATNT = "constatnt";
 
 	public static void main(String[] args) {
-		SpringApplication.run(DataColection.class, args);
+		SpringApplication.run(DataCollection.class, args);
 	}
 
 }
