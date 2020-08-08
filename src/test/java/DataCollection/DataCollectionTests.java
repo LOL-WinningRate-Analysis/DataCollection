56cbefb72f4416743d0ad07d465de49c559a3e5d
dongjae <dhehdwo7@ajou.ac.kr>
2020-08-09 08:51:03 +0900

    Setting(maven,swagger,etc.)

---
+++ src/test/java/DataCollection/DataCollectionTests.java
@@ -0,0 +1,13 @@
+package DataCollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCollectionTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
