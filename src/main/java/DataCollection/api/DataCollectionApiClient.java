0d457869587c7a78f8f98a76f81ab7b8c39c9fbd
dongjae <dhehdwo7@ajou.ac.kr>
2020-08-11 08:55:49 +0900

    Add getMatchId function

--- src/main/java/DataCollection/api/DataCollectionApiClient.java
+++ src/main/java/DataCollection/api/DataCollectionApiClient.java
@@ -1,6 +1,7 @@
 package DataCollection.api;
 
 import DataCollection.domain.LeagueEntryDto;
+import DataCollection.domain.Match;
 import DataCollection.domain.MatchDetail;
 import DataCollection.domain.Summoners;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -20,7 +22,7 @@ public class DataCollectionApiClient {
     private String ApiKey = "RGAPI-e5ab97f6-0815-4208-9c5c-d3e3d299fe72";
     private String GET_GAMEDETAIL_URI = "https://kr.api.riotgames.com/lol/match/v4/matches/{gameId}?api_key={ApiKey}";
     private String GET_USERNAME_URI="https://kr.api.riotgames.com/lol/league-exp/v4/entries/RANKED_SOLO_5x5/{tier}/{division}?page={page}&api_key={ApiKey}";
-    private String GET_GAMEIDS_URI="";
+    private String GET_GAMEIDS_URI="https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/{AccountId}?queue=420&api_key={ApiKey}";
     private String GET_ACCOUNTID_URI="https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{SummonerName}?api_key={ApiKey}";
 
     public MatchDetail getMatchDetail(long matchid){
@@ -38,7 +40,11 @@ public class DataCollectionApiClient {
         return AccountId.getAccountId();
     }
     public List<Long> getMatchIds(String accountId){
-
+        Match match = restTemplate.getForObject(GET_GAMEIDS_URI,Match.class,accountId,ApiKey);
+        List<Long> matchIds = match.getMatches().stream()
+                .map(match_ -> match_.getGameId())
+                .collect(Collectors.toList());
+        return matchIds;
     }
 
 }
