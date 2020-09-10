4f474c26bac6697c905317cecfb19aee868e779b
dongjae <dhehdwo7@ajou.ac.kr>
2020-09-11 03:11:41 +0900

    Add LineDecideAlgorithm

---
+++ src/main/java/DataCollection/Service/LineDefinitionAlgorithmService.java
@@ -0,0 +1,33 @@
+package DataCollection.Service;
+
+import DataCollection.domain.MergedData;
+import DataCollection.repository.LineDefinitionAlgorithmRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LineDefinitionAlgorithmService {
+
+    @Autowired
+    LineDefinitionAlgorithmRepository lineDefinitionAlgorithmRepository;
+
+    public MergedData informationMerge(long matchId) {
+        return lineDefinitionAlgorithmRepository.informationMerge(matchId);
+    }
+
+    public List test(MergedData mergedData){
+        return lineDefinitionAlgorithmRepository.decideJungle(mergedData);
+    }
+
+    public List test2(MergedData mergedData){
+        return lineDefinitionAlgorithmRepository.decideSupport(mergedData);
+    }
+
+    public List test3(MergedData mergedData){ return lineDefinitionAlgorithmRepository.decideBotom(mergedData);}
+
+    public List<List> getWeight(MergedData mergedData) {
+        return lineDefinitionAlgorithmRepository.getWeight(mergedData);
+    }
+}
